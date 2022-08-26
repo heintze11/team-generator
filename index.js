146cf0461e26ea2f48704037e383aaee35be0356
Levi H <levi.heintzelman@gmail.com>
2022-08-26 13:47:48 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquire = ("inquirer");
+const manager = ("./manager.js");
+const intern = ("./intern.js");
+const fs = ("fs");
+
+// Install dependencies
+// Create application to ask for Manager name, ID, Email, Office Number
+// Save manager data
+// Select another employee to add: Intern, Engineer, or finish
+// Engineer - Name, ID, Email, Github
+// Intern - Name, ID, Email, school
+// Finish building team
+    // creates HTML with cards for each employee
+    // Links to email and github
+
+    // Test each JS file - Employee, Manager, Engineer, Intern
+
