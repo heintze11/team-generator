f2f764b3776594be4962d0893a3933211d9353a1
Levi H <levi.heintzelman@gmail.com>
2022-08-28 14:30:21 -0600

    update colors for html

--- src/team.js
+++ src/team.js
@@ -4,12 +4,11 @@ const createTeam = (team) => {
     // create manager card
     const createManager = manager => {
         let managerHtml = `
-        <div class="card">
-            <div class="card-header" style="width: 18rem;">
-                ${manager.name} <br/>
-                <!-- icon -->
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${manager.name}</h5>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${manager.id}</li>
+                    <li class="list-group-item">ID #: ${manager.id}</li>
                     <li class="list-group-item">Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
                     <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
                 </ul>
@@ -20,12 +19,12 @@ const createTeam = (team) => {
     // create engineer card
     const createEngineer = engineer => {
         let engineerHtml = `
-        <div class="card">
-            <div class="card-header" style="width: 18rem;">
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
                 ${engineer.name} <br/>
                 <!-- icon -->
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${engineer.id}</li>
+                    <li class="list-group-item">ID #: ${engineer.id}</li>
                     <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
                     <li class="list-group-item">Github Username: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
                 </ul>
@@ -36,12 +35,12 @@ const createTeam = (team) => {
     // create intern cards
     const createIntern = intern => {
         let internHtml = `
-        <div class="card">
-            <div class="card-header" style="width: 18rem;">
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
                 ${intern.name} <br/>
                 <!-- icon -->
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${intern.id}</li>
+                    <li class="list-group-item">ID #: ${intern.id}</li>
                     <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
                     <li class="list-group-item">School: ${intern.school}</li>
                 </ul>
